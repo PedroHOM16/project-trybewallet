9452d57f95b327214beb2a674731e20ce3774f8c
Pedro Henrique <pedro.admoura@gmail.com>
2022-04-07 16:06:38 -0300

    requisito 8 feito

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { dataExpense } = this.props;
+    console.log('state.wallet', dataExpense);
     return (
       <table>
         <tr>
@@ -15,9 +19,51 @@ class Table extends Component {
           <th>Moeda de conversão</th>
           <th>Editar/Excluir</th>
         </tr>
+        {
+          dataExpense.length > 0
+            ? dataExpense.map((each, i) => (
+              <tr key={ i }>
+                <td>{each.description}</td>
+                <td>{each.tag}</td>
+                <td>{each.method}</td>
+                <td>{parseFloat(each.value).toFixed(2)}</td>
+                <td>{each.exchangeRates[`${each.currency}`].name}</td>
+                <td>
+                  {parseFloat(each.exchangeRates[`${each.currency}`].ask).toFixed(2)}
+                </td>
+                <td>
+                  {
+                    parseFloat(each.value * each.exchangeRates[`${each.currency}`].ask)
+                      .toFixed(2)
+                  }
+                </td>
+                <td>Real</td>
+                <td>
+                  <button
+                    type="button"
+                    data-testid="delete-btn"
+                  >
+                    Excluir
+                  </button>
+                </td>
+              </tr>
+            ))
+            : <span>Você não possui despesas cadastradas no momento</span>
+        }
       </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  dataExpense: PropTypes.shape({
+    length: PropTypes.number,
+    map: PropTypes.func,
+  }),
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  dataExpense: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
